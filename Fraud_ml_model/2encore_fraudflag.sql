0138ca788873ea38a60f4871cdcc397e83df11e9
jferragut99 <91463223+jferragut99@users.noreply.github.com>
2024-12-29 23:45:22 +0100

    Update 2encore_fraudflag.sql

--- Fraud_ml_model/2encore_fraudflag.sql
+++ Fraud_ml_model/2encore_fraudflag.sql
@@ -4,7 +4,14 @@
 ) }}
 
 -- fraud and no fraud cases in encore
-WITH last_hour AS (
+WITH first_user_appearance AS (
+SELECT RQ_BOOKING:body:"creationUser"::STRING AS creation_user ,booking_timestamp 
+FROM {{source('api_analytics','encore_raw_follow_the_booking')}}
+WHERE creation_user IS NOT NULL AND bookingreference IS NOT NULL
+QUALIFY ROW_NUMBER() OVER (PARTITION BY creation_user ORDER BY booking_timestamp )-1=0
+ORDER BY 1
+)
+, last_hour AS (
 SELECT DISTINCT 
     bookingreference,
     booking_timestamp,
@@ -12,7 +19,7 @@ SELECT DISTINCT
     product_type,
     channel,
     lh.apikey AS credential,
-    RQ_BOOKING:body:"xapplication"::string AS application,
+    COALESCE(RQ_BOOKING:body:"xapplication"::STRING,channel) AS application,
     RQ_BOOKING:body:"creationUser"::string AS creation_user,
     client_ip,
     CASE WHEN checkrate_timestamp IS NULL THEN 'No Check Rate' ELSE '' END AS no_check_rate_flag,
@@ -42,12 +49,12 @@ SELECT DISTINCT
     ecb.currency,
     ecb.paymenttype,
     ecb.username,
-    cl.CLIENT_ID ,
+    cl.client_id ,
     CASE WHEN localizador IS NULL THEN FALSE
     ELSE TRUE END AS is_fraud
-FROM hbg_api_analytics.community_source.encore_raw_follow_the_booking lh
+FROM {{source('api_analytics','encore_raw_follow_the_booking')}} lh
 LEFT JOIN (SELECT DISTINCT seqbooking, ttoo, username, bookingamount, currency, paymenttype
-            FROM hbg_api_analytics.community_source.evolution_audit_confirm_booking_operation_data_v
+            FROM {{source('api_analytics','evolution_audit_confirm_booking_operation_data_v')}}
             WHERE bookingoperation = 'Booking') ecb
 ON lh.bookingreference = ecb.seqbooking
 LEFT JOIN 
@@ -56,31 +63,38 @@ ON bookingreference=frtrue.localizador
 LEFT JOIN (SELECT DISTINCT 
                     client_xml_user,
                     COALESCE(client_main_code,client_id) client_id,
-                FROM hbg_api_analytics.community_source.encore_masterdata_clients ) cl --client_id
+                FROM {{source('api_analytics','encore_masterdata_clients')}} ) cl --client_id
 ON LOWER(credential) = LOWER(cl.client_xml_user)
 WHERE booking_timestamp::DATE between ('2023-08-01') and ('2024-10-30') -- dates where we have fraud cases labelled
 AND bookingreference IS NOT NULL 
-AND  booking_modification_flag!='S' -- not a modification, because it causes lead_time very negative
-AND  booking_modification_flag!='Y'
-AND lead_time_days <= 455 -- maximum value of the fraud cases
+--AND  booking_modification_flag!='S' -- not a modification, because it causes lead_time very negative
+--AND  booking_modification_flag!='Y'
+AND lead_time_days <= 302 -- maximum value of the fraud cases
+AND (application='evolution' OR localizador IS NOT NULL) -- only web but all from fraud
 -- just want the first appearance of the booking, without modifications
 QUALIFY ROW_NUMBER() OVER (PARTITION BY bookingreference ORDER BY booking_timestamp asc) =1
 )
+, diff_days_ipid AS(
+    SELECT  resultado, ip ,event_timestamp 
+    ,DENSE_RANK() OVER ( PARTITION BY resultado, ip ORDER BY event_timestamp ::DATE  )-1 AS different_days_ip_id
+    FROM {{source('hbg_bi_community_analytics','fct_evolution_audit_confirm_geoip_data')}}
+    WHERE ip IS NOT NULL
+    AND error='0'
+)
 SELECT last_hour.*
-    , case when channel = 'evolution' or channel = 'evolution_hotelapi' then COALESCE((SELECT count(*)
-			FROM hbg_bi.community_analytics.fct_evolution_audit_confirm_geoip_data lgweb
+, COALESCE((SELECT count(*)
+			FROM {{source('hbg_bi_community_analytics','fct_evolution_audit_confirm_geoip_data')}} lgweb
 			WHERE lgweb.resultado IS NOT NULL
             AND lgweb.resultado = last_hour.client_id::integer::string
-			AND ERROR='1'
-			AND lgweb.event_timestamp  BETWEEN DATEADD(hour,-24,booking_timestamp) AND booking_timestamp),
-			0) 
-            else NULL end  AS total_ko_web
-    , case when channel = 'XML 3' then COALESCE((SELECT sum(lg.hits)
-			FROM HBG_API_ANALYTICS.COMMUNITY_SOURCE.ENCORE_TYK_ERRORS_STATS lg
-			WHERE lg.apikey IS NOT NULL 
-            AND lg.apikey =credential
-			AND response_code='401'
-			AND lg.start_hour BETWEEN DATEADD(hour,-24,booking_timestamp) AND booking_timestamp),
-			0)
-            else null end  AS total_ko_api
+			AND error='1'
+			AND lgweb.event_timestamp  BETWEEN DATEADD(hour,-24,last_hour.booking_timestamp) AND last_hour.booking_timestamp),
+			0)   AS total_ko_web
+, fa.booking_timestamp AS first_user_appear 
+, (select max(different_days_ip_id) 
+    from diff_days_ipid dd 
+    where dd.resultado = last_hour.client_id::integer::string
+    and dd.ip = last_hour.client_ip 
+    and dd.event_timestamp<=last_hour.booking_timestamp) as different_days_ip_id
 FROM last_hour
+LEFT JOIN first_user_appearance fa
+on last_hour.creation_user = fa.creation_user
